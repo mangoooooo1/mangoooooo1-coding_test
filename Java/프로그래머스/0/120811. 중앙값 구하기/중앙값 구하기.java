f854126191a3a6de747e7b24a84882a553f78fab
mangoooooo1 <phl2510@naver.com>
2026-03-09 21:14:03 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.51 ms, Memory: 77.1 MB -BaekjoonHub

--- Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,15 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
-        int centerNum = array.length/2;
-        int length = array.length;
-        int answer = -1;
-        if (length%2!=0 && 0 < length && length < 100) {
-            if (-1000 > array[centerNum] && array[centerNum] > 1000 ) return answer;
-            answer = array[centerNum];
-        }
+        int answer = 0;
         return answer;
     }
 }
