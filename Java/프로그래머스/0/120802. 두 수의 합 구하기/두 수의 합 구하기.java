bb240b9a19f1fc86854c46e5ccef0d06e5eb58fe
mangoooooo1 <phl2510@naver.com>
2026-03-09 19:43:41 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if ( -50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
+            int answer = num1 + num2;
+            return answer;
+        } else {
+            int answer = -1;
+            return answer;
+        }
+    }
+}
