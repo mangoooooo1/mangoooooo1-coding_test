b9c7f429d0e79d49f9e54dc399ff15696752b516
mangoooooo1 <phl2510@naver.com>
2026-03-12 23:26:11 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String reverse = new StringBuilder(my_string).reverse().toString();
+        return reverse;
+    }
+}
